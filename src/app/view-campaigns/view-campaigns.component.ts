e3941261849b5afcdcfa165b43831b4dd3193f8f
KaneKopetski <KaneKopetski@users.noreply.github.com>
2020-01-09 13:45:29 -0500

    Displaying everything but image

--- src/app/view-campaigns/view-campaigns.component.ts
+++ src/app/view-campaigns/view-campaigns.component.ts
@@ -3,6 +3,11 @@ import { CampaignService} from '../campaign.service';
 import { Observable } from 'rxjs';
 import { CampaignResponse} from './campaign-response';
 import {LocalStorageService} from 'ngx-webstorage';
+import {ProfilePayload} from '../display-profile/profile-payload';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-view-campaigns',
@@ -12,12 +17,33 @@ import {LocalStorageService} from 'ngx-webstorage';
 export class ViewCampaignsComponent implements OnInit {
   campaigns: Observable<Array<CampaignResponse>>;
   username: string;
+  response: any;
 
-  constructor(private campaignService: CampaignService, private localStorageService: LocalStorageService) { }
+  constructor(private campaignService: CampaignService, private localStorageService: LocalStorageService, private httpClient: HttpClient) {
+  }
 
   ngOnInit() {
-    this.username = this.localStorageService.retrieve('username');
-    this.campaigns =  this.campaignService.getAllCampaignsForUser(this.username);
+    // this.username = this.localStorageService.retrieve('username');
+    // this.httpClient
+    //   .get('http://localhost:8080/api/campaigns/owner/' + this.username)
+    //     .subscribe(response => this.campaigns = response.data);
+    this.campaigns = this.campaignService.getAllCampaignsForUser(this.localStorageService.retrieve('username'));
   }
+
+  // ngOnInit() {
+  //   this.campaignService.getAllCampaignsForUser(this.localStorageService.retrieve('username'))
+  //     .subscribe(campaigns => this.campaigns = campaigns['results']);
+  // }
+
+  // ngOnInit() {
+  //   this.username = this.localStorageService.retrieve('username');
+  //   this.campaignService.getAllCampaignsForUser(this.username)
+  //     .subscribe((data: Array<CampaignResponse>) =>
+  //     {
+  //       this.campaigns = Array<data>;
+  //   }, (err: any) => {
+  //     console.log('Failure Response');
+  //   });
+  // }
 
 }
